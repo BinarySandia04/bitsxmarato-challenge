235a21b0de4fa860793b064209c979f3f72a3533
BinarySandia04 <aranseraroig@gmail.com>
2023-12-15 22:44:20 +0100

    Firsto comimto

---
+++ sex-roguelike/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+import StartPrompt from '@/components/StartPrompt.vue'
+
+</script>
+
+<template>
+  <StartPrompt></StartPrompt>
+</template>
+
+<style>
+h1 {
+  font-size: 200px;
+}
+</style>
---
+++ sex-roguelike/src/views/TestView.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+<h1>
+    Hola mundo!
+</h1>
+</template>
+
+<style>
+h1 {
+    color: rgb(255, 123, 176)violet;
+}
+</style>
